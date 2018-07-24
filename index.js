45abee87ec4f96871188a97333a824b5b339ba6b
Trey Allen <treyallen@Treys-MacBook-Pro.local>
2018-07-24 15:48:34 -0500

    This are my first files in github

---
+++ index.js
@@ -0,0 +1,13 @@
+//This prints a message on the console
+console.log("This should work");
+
+/*
+    This is a comment
+Another one here
+    Another one here
+*/
+
+let  someNumber = 8*2;
+console.log(someNumber+4);
+
+
